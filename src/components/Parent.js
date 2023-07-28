e37987b8dabbfd3b0eeea14f98c873db9a44218c
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-07-28 22:54:13 +0530

    Update 1

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-import Child from "./Child";
 
 const Parent = () => {
 	const [showModal, setShowModal] = useState(false);
 	return (
 		<div>
 			<h1>Parent Component</h1>
-			<Child setShowModal={setShowModal} showModal={showModal} />
+			<h3>Child Component</h3>
+			<button onClick={() => setShowModal((prev) => !prev)}>Show MOdal</button>
+			{showModal && (
+				<div>
+					<h3>Modal Content</h3>
+					<p>This is the modal content.</p>
+				</div>
+			)}
 		</div>
 	);
 };
